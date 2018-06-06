f43daa5531defd2656fd068b5584c6d7e1b2bf57
Dennis Hübner <dennis.huebner89@gmail.com>
2018-06-06 12:32:35 +0200

    Initial Commit

---
+++ LeagueiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeagueiOS
+//
+//  Created by Dennis on 06.06.18.
+//  Copyright © 2018 Dennis Hübner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
